273d02001f5239752263fc9bdd4d503c6eb196a6
abdu951 <abdumoh051@gmail.com>
2025-12-11 17:56:59 +0300

    practice routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http')
+const path = require('path')
 const url = require('url')
+const querystring = require('querystring')
 
 /*const server = http.createServer((req, res) => {
 
@@ -51,7 +53,7 @@ const port = 3000
 
 server.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`)
-})*/
+})
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET' & req.url.startsWith('/search')) {
@@ -74,3 +76,106 @@ const port = 3000
 server.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`)
 })
+
+const routes = {
+    '/': (req, res) => {
+        res.writeHead(200,{'content-type': 'text-plain'})
+        res.end('welcome to homepage')
+    },
+
+    '/about': (req, res) => {
+        res.writeHead(200,{'content-type': 'text-plain'})
+        res.end('welcome to aboutpage')
+    },
+
+    '/notfound': (req, res) => {
+        res.writeHead(404,{'content-type': 'text-plain'})
+        res.end('page not found')
+    }
+}
+
+const server = http.createServer((req, res) => {
+    const { pathname } = url.parse(req.url)
+    if (routes[pathname]) {
+        routes[pathname](req, res)
+    } else {
+        routes['/notfound'](req, res)
+    }
+})
+
+const port = 3000
+
+server.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`)
+})
+
+const server = http.createServer((req, res) => {
+    const { pathname } = url.parse(req.url)
+    if (pathname.startsWith('/user/')) {
+        const userid = pathname.split('/')[2]
+        res.writeHead(200, {'content-type': 'text/plain'})
+        res.end(`user id : ${userid}`)
+    } else {
+        res.writeHead(404, {'content-type': 'text/plain'})
+        res.end('routes not found')
+    }
+})
+
+const port = 3000
+
+server.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`)
+})
+
+function logrequest(req,res,next) {
+    console.log(`${req.method} request made to ${req.url}`)
+    next(req,res)
+}
+const server = http.createServer((req, res) => {
+    logrequest(req,res,(req,res) => {
+        const { pathname } = url.parse(req.url)
+    if (pathname.startsWith('/user/')) {
+        const userid = pathname.split('/')[2]
+        res.writeHead(200, {'content-type': 'text/plain'})
+        res.end(`user id : ${userid}`)
+    } else {
+        res.writeHead(404, {'content-type': 'text/plain'})
+        res.end('routes not found')
+    }
+     
+    })
+    
+})
+
+const port = 3000
+
+server.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`)
+})*/
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST' & req.url === '/submit') {
+        let data = ''
+
+        req.on('data', (chunk) => {
+            data += chunk
+        })
+
+        req.on('end', () => {
+            const parsedData = querystring.parse(data)
+            res.writeHead(200,{'content-type': 'application/JSON'})
+            res.end(JSON.stringify({message: 'form data recieved', parsedData}))
+        })
+    }else {
+        res.writeHead(404, {'content-type': 'text/plain'})
+        res.end('page not found')
+    }
+
+})
+
+const port = 3000
+
+server.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`)
+})
+  

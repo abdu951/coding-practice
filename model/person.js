ceb4b2d92abb96d612ccd8dc98b1ed3e67eb6ef0
abdu951 <abdumoh051@gmail.com>
2025-12-19 23:05:51 +0300

    return schema to first structure

--- model/person.js
+++ model/person.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
 
 const personSchema = new mongoose.Schema({
-    fname: {type: String, required: true},
-    lname: {type: String, required: true},
+    name: {type: String, required: true},
     age: {type: Number, required: true},
     email: {type: String, required: true, unique: true},
     userOrder: {type: Object, default:{} }

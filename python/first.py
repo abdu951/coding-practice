e2752fcb5fff8afb989ab4acb3e96cef9889a5e8
abdu951 <abdumoh051@gmail.com>
2026-01-11 00:53:26 +0300

    recheck some part

--- python/first.py
+++ python/first.py
@@ -1,12 +1,12 @@
 # fundamentals of python
-"""print('hello world')
+import math
+print('hello world')
 print('*' * 10)
 2 + 3
 student = ['zhangsan', 'lisi', 'wangwu']
 if 'zhangsan' in student:
     print('yes')
 print(student)
-
 
 
 # strings and some properties like length, index slicing
@@ -18,19 +18,19 @@ print(course[-1])
 print(course[0:3])
 print(course[:3])
 print(course[3:])
-print(course[:]) 
+print(course[:])
 
 # escape sequence in python
 print('hello \"world')
 print('hello \'world')
 print('hello \\world')
-print('hello \nworld') 
+print('hello \nworld')
 
 # formated strings in python
 first = 'zhangsan'
 last = 'lisi'
 full = f'hello {first} {last}'
-print(full)  
+print(full)
 
 # string methods in python
 course = "python programming"
@@ -40,7 +40,7 @@ print(course.title())
 print(course.find('pr'))
 print(course.replace('p', 'j'))
 print('python' in course)
-print('python' not in course) 
+print('python' not in course)
 
 # numbers in python
 
@@ -61,18 +61,18 @@ print(x % y)
 print(x ** y)
 
 # augmented assignment
-x = x+y  # x += y are equal  
+x = x+y  # x += y are equal
 
 # working with numbers
-import math
 
-print(math.ceil(2.4)) # which means the smallest integer >= 2.4
+print(math.ceil(2.4))  # which means the smallest integer >= 2.4
 print(math.floor(2.4))
+# abc: means Absolute value = distance from zero, it remove the negative sign because disance always posetive
 print(abs(-2.4))
 print(pow(2, 3))
 print(max(2, 3))
 print(min(2, 3))
-print(round(2.4)) 
+print(round(2.4))
 
 # type conversion in python
 x = 10
@@ -83,15 +83,15 @@ print(int('10') + int('3'))
 
 # input in python
 name = input('enter your name: ')
-print('hello ' + name) 
+print('hello ' + name)
 
 # falsy value in python
-# False, None, 0, "", 
+# False, None, 0, "",
 bool(0)
 bool(None)
 bool('')
 # truthy value in python
-# True, "0", " "          
+# True, "0", " "
 
 # fundamentals of programming
 
@@ -106,8 +106,10 @@ print(a >= b)
 print(a <= b)
 
 # another comparison operators
-print("bag" > "apple")  # True: i will come back to this
-print("bag" > "BAG")  # True: the numeric representation of "b" is 98, the numeric representation of "B" is 66
+print("bag" > "apple")  # True: because Strings are compared alphabetically
+
+# True: the numeric representation of "b" is 98, the numeric representation of "B" is 66
+print("bag" > "BAG")
 
 
 # conditional statements in python: conditional statements are indentation or spaces sensetive
@@ -120,12 +122,12 @@ elif temperature > 20:
     print("it's nice")
 else:
     print("it's cold")
-print("done") 
+print("done")
 
 # ternery operator in python: i will come back to this how to add elif statement
 temperature = 30
-message = "it's hot" if temperature > 30 else "it's not hot"
-print(message) 
+message = "it's hot" if temperature > 30 else "it's moderate" if temperature > 20 else "it's not hot"
+print(message)
 
 # short-circuit evaluation: in python logical operators are short-circuit evaluated which means if the first operand is false then the second operand will not be evaluated
 # logical operators in python
@@ -146,7 +148,7 @@ else:
 if not student:
     print("eligible")
 else:
-    print("not eligible") 
+    print("not eligible")
 
 # chaining comparison operators in python
 # age should be between 18 and 65
@@ -154,7 +156,7 @@ age = 22
 if 18 <= age < 65:
     print("eligible")
 else:
-    print("not eligible")  
+    print("not eligible")
 
 # quiz question
 
@@ -163,11 +165,11 @@ if 10 == "10":
 elif "bag" > "apple" and "bag" > "cat":
     print('b')
 else:
-    print('c') 
+    print('c')
 
 # for loop in python
 for i in range(1, 10, 2):
-    print("attempt", i, i * ".") 
+    print("attempt", i, i * ".")
 
 # for...else loop in python
 successful = True
@@ -177,25 +179,25 @@ for i in range(3):
         print("successful")
         break
 else:
-    print("failed after 3 attempts") 
+    print("failed after 3 attempts")
 
 # nested loop in python
 for i in range(1, 4):
     for j in range(1, 4):
-        print(i, j)  
+        print(i, j)
 
 # iterables in python
 for x in [1, 2, 3, 4, 5]:
     print(x)
 
 for item in 'shopning_list':
-    print(item)  
+    print(item)
 
 # while loop in python
 i = 1
 while i <= 10:
     print(i)
-    i += 1  
+    i += 1
 
 # infinite loop in python: is running until the condition is false
 i = 1
@@ -203,7 +205,7 @@ while True:
     print(i)
     i += 1
     if i > 10:
-        break 
+        break
 # quiz question: show the even numbers from 1 to 10
 i = 2
 while True:
@@ -219,10 +221,12 @@ for number in range(1, 10):
     if number % 2 == 0:
         count += 1
         print(number)
-print(f"we have {count} even numbers") 
+print(f"we have {count} even numbers")
 
 # in python all functions return none value by default
 # functions in python
+
+
 def greet():
     print("hello")
     print("how are you")
@@ -237,15 +241,17 @@ greet()
 def greet(first_name, last_name):
     print(f"hello {first_name} {last_name}")
     print("how are you")
-    print("i am fine") 
+    print("i am fine")
 
 
 greet("abdu", "mame")
-greet("seya", "mame")   
+greet("seya", "mame")
 
 # types of functions in python: in programming we have two types of functions 1, perform a task 2, return a value
 
 # perform a task function
+
+
 def greet(name):
     print(f"hello {name}")
 
@@ -253,19 +259,23 @@ def greet(name):
 greet("abdu")
 
 # return a value function
+
+
 def square(number):
     return number * number
 
 
 result = square(3)
-print(result)  
+print(result)
 
 # keyword arguments in python
+
+
 def increment(number, by):
     return number + by
 
 
-print(increment(number=3, by=2)) # this is keyword arguments 
+print(increment(number=3, by=2))  # this is keyword arguments
 
 # default arguments in python: all the optional arguments should be at the end
 
@@ -274,12 +284,12 @@ def increment(number=2, by=1):  # this is default arguments
     return number + by
 
 
-print(increment(3, 2)) """
+print(increment(3, 2))
 
-# args, wait, what
+# args, wait, in this num times two is the first argument and through number the value is changed by the first result
 
 
-def multiply(*number):
+def multiply(*number):  # *number: means Accept any number of arguments and Store them as a tuple like this(2, 3, 4, 5)
     total = 2
     for num in number:
         total *= num

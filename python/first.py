41a6f74a98d795a1ece087b8e00c967171bdb8a0
abdu951 <abdumoh051@gmail.com>
2026-01-10 11:36:04 +0300

    practice some properties

--- python/first.py
+++ python/first.py
@@ -1,7 +1,16 @@
-print('hello world')
+"""print('hello world')
 print('*' * 10)
 2 + 3
 student = ['zhangsan', 'lisi', 'wangwu']
 if 'zhangsan' in student:
     print('yes')
-print(student)
+print(student)"""
+course = "python programming"
+length = len(course)
+print(length)
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[:3])
+print(course[3:])
+print(course[:])

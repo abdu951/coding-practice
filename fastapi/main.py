44e6c046503f43a70e9ae3b729c2f9a9ce7c8aee
abdu951 <abdumoh051@gmail.com>
2026-01-12 22:52:20 +0300

    add post api

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
+
+
+items = []
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post('/items')
+def create_item(item: Item):
+    items.append(item)
+    return items

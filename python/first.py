545f5675e43a887d30c0a504b3dc5b2065489d06
abdu951 <abdumoh051@gmail.com>
2026-01-12 22:44:36 +0300

    something includes in quote

--- python/first.py
+++ python/first.py
@@ -1,5 +1,5 @@
 # fundamentals of python
-import math
+"""import math
 print('hello world')
 print('*' * 10)
 2 + 3
@@ -296,4 +296,18 @@ def multiply(*number):  # *number: means Accept any number of arguments and Stor
     return total
 
 
-print(multiply(2, 3, 4, 5))
+print(multiply(2, 3, 4, 5))  """
+
+import random
+
+while True:
+    choice = input('Roll the dice? (y/n): ').lower()
+    if choice == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'({die1}, {die2})')
+    elif choice == 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print('Invalid choice!')

d3d1ce1bf202e21652951f03e4302d157e58da8f
abdu951 <abdumoh051@gmail.com>
2026-01-20 09:53:18 +0300

    move the code

--- python/first.py
+++ python/first.py
@@ -297,17 +297,3 @@ def multiply(*number):  # *number: means Accept any number of arguments and Stor
 
 
 print(multiply(2, 3, 4, 5))  """
-
-import random
-
-while True:
-    choice = input('Roll the dice? (y/n): ').lower()
-    if choice == 'y':
-        die1 = random.randint(1, 6)
-        die2 = random.randint(1, 6)
-        print(f'({die1}, {die2})')
-    elif choice == 'n':
-        print('Thanks for playing!')
-        break
-    else:
-        print('Invalid choice!')

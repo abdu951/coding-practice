812c86a31c75f0b10a6c010a5a30fc4e0d4eb28a
abdu951 <abdumoh051@gmail.com>
2026-01-21 21:38:36 +0300

    modify the code using DRY principal

--- python_project/app.py
+++ python_project/app.py
@@ -36,8 +36,8 @@ while True:
 
 
 # rock, paper, scissors game
-choices = ('r', 'p', 's')
 emojis = {'p': '📄', 's': '✂', 'r': '🌑'}  # this are python dictionary method.
+choices = ('r', 'p', 's')
 
 while True:
     user_choice = input('rock, paper, or scissors? (r/p/s): ').lower()
@@ -66,9 +66,14 @@ while True:
     if should_continue == 'n':
         print('Thanks for playing!')
         break   """
+# USING DRY PRINCIPLE which menas don't repeat yourself so declare the variables as constants and use them every where in the program
+ROCK = 'r'
+PAPER = 'p'
+SCISSORS = 's'
 
-choices = ('r', 'p', 's')
-emojis = {'p': '📄', 's': '✂', 'r': '🌑'}  # this are python dictionary method.
+# this are python dictionary method.
+emojis = {PAPER: '📄', SCISSORS: '✂', ROCK: '🌑'}
+choices = tuple(emojis.keys())
 
 
 def get_user_choice():
@@ -90,9 +95,9 @@ def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print('Tie!')
     elif (
-        (user_choice == 'r' and computer_choice == 's') or
-        (user_choice == 's' and computer_choice == 'p') or
-        (user_choice == 'p' and computer_choice == 'r')
+        (user_choice == ROCK and computer_choice == SCISSORS) or
+        (user_choice == SCISSORS and computer_choice == PAPER) or
+        (user_choice == PAPER and computer_choice == ROCK)
     ):
         print('you win!')
     else:

1e99377ca5c257f8a57b3818b3fcf5c9745f8f2b
abdu951 <abdumoh051@gmail.com>
2026-01-21 14:09:48 +0300

    refactoring the code by modularization

--- python_project/app.py
+++ python_project/app.py
@@ -32,7 +32,7 @@ while True:
             print('Congratulations! You guessed the number.')
             break
     except ValueError:
-        print('Please enter a valid input.')  """
+        print('Please enter a valid input.')  
 
 
 # rock, paper, scissors game
@@ -65,4 +65,54 @@ while True:
     should_continue = input('do you want to continue? (y/n): ').lower()
     if should_continue == 'n':
         print('Thanks for playing!')
-        break
+        break   """
+
+choices = ('r', 'p', 's')
+emojis = {'p': '📄', 's': '✂', 'r': '🌑'}  # this are python dictionary method.
+
+
+def get_user_choice():
+    while True:
+        user_choice = input('rock, paper, or scissors? (r/p/s): ').lower()
+
+        if user_choice in choices:
+            return user_choice
+        else:
+            print('invalid choice!')
+
+
+def display_choices(user_choice, computer_choice):
+    print(f'you choose: {emojis[user_choice]}')
+    print(f'computer choose: {emojis[computer_choice]}')
+
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print('Tie!')
+    elif (
+        (user_choice == 'r' and computer_choice == 's') or
+        (user_choice == 's' and computer_choice == 'p') or
+        (user_choice == 'p' and computer_choice == 'r')
+    ):
+        print('you win!')
+    else:
+        print('you lose!')
+
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        should_continue = input('do you want to continue? (y/n): ').lower()
+        if should_continue == 'n':
+            print('Thanks for playing!')
+            break
+
+
+play_game()

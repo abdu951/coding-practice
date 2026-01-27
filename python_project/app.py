b0539ddd7c0367632a4cb56946e09f7e403f2675
abdu951 <abdumoh051@gmail.com>
2026-01-27 23:04:13 +0300

    remove if statement

--- python_project/app.py
+++ python_project/app.py
@@ -26,10 +26,6 @@ while True:
  # dice rolling game   
     while True:
     choice = input('Roll the dice? (y/n): ').lower()
-    if choice == 'y':
-        die1 = random.randint(1, 6)
-        die2 = random.randint(1, 6)
-        print(f'({die1}, {die2})')
     
      
 

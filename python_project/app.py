8e9d1a73fa3ae79e77dd220e3400cd824102ae71
abdu951 <abdumoh051@gmail.com>
2026-01-28 23:29:07 +0300

    remove try also

--- python_project/app.py
+++ python_project/app.py
@@ -6,7 +6,7 @@ import random
 secret_number = random.randint(1, 100)
 
 while True:
-    try:
+   
 
         guess = int(input('guess the number between 1 and 100: '))
         if guess < secret_number and guess > 1:

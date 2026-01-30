ed56584e07d54f29edaf25b255d8d0b6cf80807e
abdu951 <abdumoh051@gmail.com>
2026-01-30 23:20:57 +0300

    remove else

--- python_project/app.py
+++ python_project/app.py
@@ -15,9 +15,7 @@ while True:
             print('Your guess is too high.')
         elif guess < 1 or guess > 100:
             print('Please enter a number between 1 and 100.')
-        else:
-            print('Congratulations! You guessed the number.')
-            
+        
             
             
         

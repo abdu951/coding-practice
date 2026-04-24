17548e204e1669ed3c46002c604129e260daf6ff
abdu951 <abdumoh051@gmail.com>
2026-04-24 23:04:04 +0300

    c

--- python_project/app.py
+++ python_project/app.py
@@ -3,50 +3,6 @@ import random
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
           """
 # USING DRY PRINCIPLE which menas don't repeat yourself so declare the variables as constants and use them every where in the program
 ROCK = 'r'
